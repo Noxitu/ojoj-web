0d583b87d7b9c8549f4a9771feeb67a85f5e82a6
Grzesiu <grzesiu@example.com>
2014-11-06 10:44:37 +0100

    Gemfile and database

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "widgets", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "stock"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
